8d87855b7f317e1a6d85b6e0a7e8f9204463dd0a
LeonidasVS <villafrancosibrian@gmail.com>
2024-08-24 08:52:04 -0600

    Guia #12 Finalizada

--- CapaConexion/Form1.cs
+++ CapaConexion/Form1.cs
@@ -84,7 +84,11 @@ namespace CapaConexion
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string cadenaConexion = DatosLayer.DataBase.ConnectionString();
+            DatosLayer.DataBase.ApplicationName = "Programacion 2 ejemplo";
+            DatosLayer.DataBase.ConnetionTimeout = 30;
+            string cadenaConexion = DatosLayer.DataBase.ConnectionString;
+
+            var conectarDB = DatosLayer.DataBase.GetSqlConnection();
         }
     }
 }
--- DatosLayer/DataBase.cs
+++ DatosLayer/DataBase.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DatosLayer
 {
     public class DataBase
     {
+        public static int ConnetionTimeout { get; set; }
+        public static string ApplicationName { get; set; }
         public static string ConnectionString
         {
             get
             {
-                return ConfigurationManager.ConnectionStrings["NWConnection"].ConnectionString;
+                string CadenaConexion= ConfigurationManager.ConnectionStrings["NWConnection"].ConnectionString;
+                SqlConnectionStringBuilder conexionBuilder =new SqlConnectionStringBuilder(CadenaConexion);
+
+                conexionBuilder.ApplicationName =
+                ApplicationName ?? conexionBuilder.ApplicationName;
+                conexionBuilder.ConnectTimeout = (ConnetionTimeout > 0)
+                    ? ConnetionTimeout : conexionBuilder.ConnectTimeout;
+                return conexionBuilder.ToString();
+
+
             }
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+
+            SqlConnection conexion = new SqlConnection(ConnectionString);
+            conexion.Open();
+            return conexion;
         }
     }
 }
