6fa0053e349c48fdae14c69046ba8531c7f70bd9
LeonidasVS <villafrancosibrian@gmail.com>
2024-08-24 06:47:30 -0600

    Guia #6 Finalizada

---
+++ CapaConexion/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-5R5EQO8\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;");
+            conexion.Open();
+            MessageBox.Show("Conectado");
+            conexion.Close();
+            MessageBox.Show("Gracias, Conexion finalizada");
+        }
+    }
+}
